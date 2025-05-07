7e0ba8564b5eb65cf1908ebaf9c36982429bec04
alvaro944 <alvaro94cervantes@gmail.com>
2025-05-07 02:53:48 +0200

    Mejoras visuales, ajuste de pantalla correcto respetando barra inferior y superior

--- app/src/main/java/com/alvarocervantes/fittrackplus/MainActivity.kt
+++ app/src/main/java/com/alvarocervantes/fittrackplus/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.alvarocervantes.fittrackplus
 
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.alvarocervantes.fittrackplus.databinding.ActivityMainBinding
@@ -19,8 +24,15 @@ class MainActivity : AppCompatActivity() {
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
 
         val navController = navHostFragment.navController
-
         binding.bottomNavView.setupWithNavController(navController)
+
+        // Ajustar dinámicamente el espaciador de la barra de estado
+        val headerRoot = findViewById<View>(R.id.viewStatusSpacer)
+        ViewCompat.setOnApplyWindowInsetsListener(headerRoot) { view, insets ->
+            val topInset = insets.getInsets(WindowInsetsCompat.Type.statusBars()).top
+            view.layoutParams.height = topInset
+            view.requestLayout()
+            insets
+        }
     }
 }
-
